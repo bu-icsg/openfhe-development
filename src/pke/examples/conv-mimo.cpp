98572f08c1f30f2c295520f65d9c9e9b67ab885e
seydang <53308177+seydang@users.noreply.github.com>
2024-10-16 15:06:27 -0400

    conv-mimo updated
    Generalized scenario.

--- src/pke/examples/conv-mimo.cpp
+++ src/pke/examples/conv-mimo.cpp
@@ -17,7 +17,7 @@ ENN ENN;
 int main() {
     cout << fixed;
     cout << setprecision(0);
-    int numSlots = 64;
+    int numSlots = 32;
 
     CCParams<CryptoContextCKKSRNS> parameters;
     parameters.SetSecurityLevel(HEStd_128_classic);
@@ -29,52 +29,6 @@ int main() {
 
     CryptoContext<DCRTPoly> cc = GenCryptoContext(parameters);
     cc->Enable(PKE);
-
-    //    vector<double> x1 = {0,2,3,0,2,3,4,5,3,0,0,2,6,1,0,1};
-    int iw = 4;
-    int ih = 4;
-    int cn = numSlots / (iw*ih);
-
-    vector<vector<double>> x_inputs;
-    vector<double> x1 = {0,2,3,0,2,3,4,5,3,0,0,2,6,1,0,1};
-    vector<double> x2 = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
-    x_inputs.push_back(x1);
-    x_inputs.push_back(x2);
-
-    vector<double> x_input;
-
-//    int fh = 3;
-//    int fw = 3;
-
-    for (int i=0; i<cn/x_inputs.size(); i++){
-        for (int j=0; j<x_inputs.size(); j++){
-            x_input.insert(x_input.end(), x_inputs[j].begin(), x_inputs[j].end());
-        }
-    }
-
-    vector<vector<double>> weights;
-    weights.push_back({1,2,1,0,1,1,2,1,1});
-    weights.push_back({-1,-2,-1,0,-1,-1,-2,-1,-1});
-    weights.push_back({1,2,1,0,1,1,2,1,1});
-    weights.push_back({-1,-2,-1,0,-1,-1,-2,-1,-1});
-    vector<double> weights_input;
-    for (int i=0; i<cn/weights.size(); i++){
-        for (int j=0; j<weights.size(); j++){
-            weights_input.insert(weights_input.end(), weights[j].begin(), weights[j].end());
-        }
-    }
-
-    int ci_t = x_inputs.size();
-    int co_t = weights.size();
-    int ci = ceil((double)ci_t / cn);
-    int co = ceil((double)co_t / cn);
-
-//    weights.push_back({1,2,1,0,1,1,2,1,1});
-//    weights.push_back({-1,-2,-1,0,-1,-1,-2,-1,-1});
-
-    Ptxt pt1 = cc->MakeCKKSPackedPlaintext(x_input);
-    pt1->SetLength(numSlots);
-
     cc->Enable(KEYSWITCH);
     cc->Enable(LEVELEDSHE);
     cc->Enable(ADVANCEDSHE);
@@ -82,65 +36,124 @@ int main() {
     auto keys = cc->KeyGen();
     cc->EvalMultKeyGen(keys.secretKey);
 
-    Ctxt ct1 = cc->Encrypt(keys.publicKey, pt1);
+    //    vector<double> x1 = {0,2,3,0,2,3,4,5,3,0,0,2,6,1,0,1};
+    int iw = 4;
+    int ih = 4;
+    int cn = numSlots / (iw*ih);
 
-    vector<vector<double>> weight_list{9};
-    for (int j=0; j<ci; j++){
-        for(int i=0; i < weights.size(); i++){
-            weight_list[0].insert(weight_list[0].end(), {0, 0, 0, 0, 0, weights[i][0], weights[i][0], weights[i][0], 0, weights[i][0], weights[i][0], weights[i][0], 0, weights[i][0], weights[i][0], weights[i][0]});
-            weight_list[1].insert(weight_list[1].end(), {0, 0, 0, 0, weights[i][1], weights[i][1], weights[i][1], weights[i][1], weights[i][1], weights[i][1], weights[i][1], weights[i][1], weights[i][1], weights[i][1], weights[i][1], weights[i][1]});
-            weight_list[2].insert(weight_list[2].end(), {0, 0, 0, 0, weights[i][2], weights[i][2], weights[i][2], 0, weights[i][2], weights[i][2], weights[i][2], 0, weights[i][2], weights[i][2], weights[i][2], 0});
-            weight_list[3].insert(weight_list[3].end(), {0, weights[i][3], weights[i][3], weights[i][3], 0, weights[i][3], weights[i][3], weights[i][3], 0, weights[i][3], weights[i][3], weights[i][3], 0, weights[i][3], weights[i][3], weights[i][3]});
-            weight_list[4].insert(weight_list[4].end(), {weights[i][4], weights[i][4], weights[i][4], weights[i][4], weights[i][4], weights[i][4], weights[i][4], weights[i][4], weights[i][4], weights[i][4], weights[i][4], weights[i][4], weights[i][4], weights[i][4], weights[i][4], weights[i][4]});
-            weight_list[5].insert(weight_list[5].end(), {weights[i][5], weights[i][5], weights[i][5], 0, weights[i][5], weights[i][5], weights[i][5], 0, weights[i][5], weights[i][5], weights[i][5], 0, weights[i][5], weights[i][5], weights[i][5], 0});
-            weight_list[6].insert(weight_list[6].end(), {0, weights[i][6], weights[i][6], weights[i][6], 0, weights[i][6], weights[i][6], weights[i][6], 0, weights[i][6], weights[i][6], weights[i][6], 0, 0, 0, 0});
-            weight_list[7].insert(weight_list[7].end(), {weights[i][7], weights[i][7], weights[i][7], weights[i][7], weights[i][7], weights[i][7], weights[i][7], weights[i][7], weights[i][7], weights[i][7], weights[i][7], weights[i][7], 0, 0, 0, 0});
-            weight_list[8].insert(weight_list[8].end(), {weights[i][8], weights[i][8], weights[i][8], 0, weights[i][8], weights[i][8], weights[i][8], 0, weights[i][8], weights[i][8], weights[i][8], 0, 0, 0, 0, 0});
+    vector<vector<double>> x_s;
+    x_s.push_back({0,2,3,0,2,3,4,5,3,0,0,2,6,1,0,1});
+    x_s.push_back({1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1});
+    x_s.push_back({1,0,6,0,2,0,0,5,6,0,1,2,1,0,0,1});
+    x_s.push_back({-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1});
+
+    vector<vector<double>> weights;
+    weights.push_back({1,2,1,0,1,1,2,1,1});
+    weights.push_back({-1,-2,-1,0,-1,-1,-2,-1,-1});
+    weights.push_back({0,0,1,0,1,0,1,0,0});
+    weights.push_back({-1,-1,-1,-1,-1,-1,-1,-1,-1});
+    weights.push_back({0,0,1,0,1,0,1,0,0});
+    weights.push_back({-1,-1,-1,-1,-1,-1,-1,-1,-1});
+    //    weights.push_back({-1,-1,-1,-1,-1,-1,-1,-1,-1});
+
+    // Perform the convolution
+    conv2d(x_s, weights, 1);
+
+    int ci = x_s.size();
+    int co = weights.size();
+    int ci_cn = ceil((double)ci / cn);
+    int co_cn = ceil((double)co / cn);
+
+    vector<vector<double>> x_inputs(ci_cn);
+    for (int i=0; i<ci_cn; i++){
+        for (int j=0; j<cn; j++){
+            x_inputs[i].insert(x_inputs[i].end(), x_s[i*cn+j].begin(), x_s[i*cn+j].end());
         }
     }
 
+    //    for (int i=0; i<ci_cn; i++){
+    //        cout << "pt: " << x_inputs[i] << endl;
+    //    }
 
-    cc->EvalRotateKeyGen(keys.secretKey, {-5,-4,-3,-1,1,3,4,5,ih*iw});
-
-    vector<vector<Ctxt>> c_rotations{9};
-    for (int i=0; i<ci_t; i++){
-        c_rotations[i].push_back(cc->EvalRotate(ct1, -5));
-        c_rotations[i].push_back(cc->EvalRotate(ct1, -4));
-        c_rotations[i].push_back(cc->EvalRotate(ct1, -3));
-        c_rotations[i].push_back(cc->EvalRotate(ct1, -1));
-        c_rotations[i].push_back(ct1);
-        c_rotations[i].push_back(cc->EvalRotate(ct1, 1));
-        c_rotations[i].push_back(cc->EvalRotate(ct1, 3));
-        c_rotations[i].push_back(cc->EvalRotate(ct1, 4));
-        c_rotations[i].push_back(cc->EvalRotate(ct1, 5));
-        ct1 = cc->EvalRotate(ct1, ih*iw);
+    vector<Ctxt> ct_inputs;
+    for (int i=0; i<ci_cn; i++){
+        Ptxt temp = cc->MakeCKKSPackedPlaintext(x_inputs[i]);
+        ct_inputs.push_back(cc->Encrypt(keys.publicKey, temp));
     }
 
-    vector<Ctxt> inter_out_ct;
-    for (int j = 0; j < co; j++) {
-        vector<Ctxt> inter_in_ct;
-        for(int l=0; l < ci_t; l++){
-            vector<Ctxt> conv_ct;
-            for (int k = 0; k < 9; k++) {
-                Ptxt weight_pt = cc->MakeCKKSPackedPlaintext(weight_list[k]);
-                Ctxt cMult     = cc->EvalMult(weight_pt, c_rotations[l][k]);
-                conv_ct.push_back(cMult);
+
+    vector<vector<vector<double>>> weight_list(co_cn, vector<vector<double>>(9));
+    for (int j=0; j<co_cn; j++){
+        for(int i=0; i < cn; i++){
+            weight_list[j][0].insert(weight_list[j][0].end(), {0, 0, 0, 0, 0, weights[i+j*cn][0], weights[i+j*cn][0], weights[i+j*cn][0], 0, weights[i+j*cn][0], weights[i+j*cn][0], weights[i+j*cn][0], 0, weights[i+j*cn][0], weights[i+j*cn][0], weights[i+j*cn][0]});
+            weight_list[j][1].insert(weight_list[j][1].end(), {0, 0, 0, 0, weights[i+j*cn][1], weights[i+j*cn][1], weights[i+j*cn][1], weights[i+j*cn][1], weights[i+j*cn][1], weights[i+j*cn][1], weights[i+j*cn][1], weights[i+j*cn][1], weights[i+j*cn][1], weights[i+j*cn][1], weights[i+j*cn][1], weights[i+j*cn][1]});
+            weight_list[j][2].insert(weight_list[j][2].end(), {0, 0, 0, 0, weights[i+j*cn][2], weights[i+j*cn][2], weights[i+j*cn][2], 0, weights[i+j*cn][2], weights[i+j*cn][2], weights[i+j*cn][2], 0, weights[i+j*cn][2], weights[i+j*cn][2], weights[i+j*cn][2], 0});
+            weight_list[j][3].insert(weight_list[j][3].end(), {0, weights[i+j*cn][3], weights[i+j*cn][3], weights[i+j*cn][3], 0, weights[i+j*cn][3], weights[i+j*cn][3], weights[i+j*cn][3], 0, weights[i+j*cn][3], weights[i+j*cn][3], weights[i+j*cn][3], 0, weights[i+j*cn][3], weights[i+j*cn][3], weights[i+j*cn][3]});
+            weight_list[j][4].insert(weight_list[j][4].end(), {weights[i+j*cn][4], weights[i+j*cn][4], weights[i+j*cn][4], weights[i+j*cn][4], weights[i+j*cn][4], weights[i+j*cn][4], weights[i+j*cn][4], weights[i+j*cn][4], weights[i+j*cn][4], weights[i+j*cn][4], weights[i+j*cn][4], weights[i+j*cn][4], weights[i+j*cn][4], weights[i+j*cn][4], weights[i+j*cn][4], weights[i+j*cn][4]});
+            weight_list[j][5].insert(weight_list[j][5].end(), {weights[i+j*cn][5], weights[i+j*cn][5], weights[i+j*cn][5], 0, weights[i+j*cn][5], weights[i+j*cn][5], weights[i+j*cn][5], 0, weights[i+j*cn][5], weights[i+j*cn][5], weights[i+j*cn][5], 0, weights[i+j*cn][5], weights[i+j*cn][5], weights[i+j*cn][5], 0});
+            weight_list[j][6].insert(weight_list[j][6].end(), {0, weights[i+j*cn][6], weights[i+j*cn][6], weights[i+j*cn][6], 0, weights[i+j*cn][6], weights[i+j*cn][6], weights[i+j*cn][6], 0, weights[i+j*cn][6], weights[i+j*cn][6], weights[i+j*cn][6], 0, 0, 0, 0});
+            weight_list[j][7].insert(weight_list[j][7].end(), {weights[i+j*cn][7], weights[i+j*cn][7], weights[i+j*cn][7], weights[i+j*cn][7], weights[i+j*cn][7], weights[i+j*cn][7], weights[i+j*cn][7], weights[i+j*cn][7], weights[i+j*cn][7], weights[i+j*cn][7], weights[i+j*cn][7], weights[i+j*cn][7], 0, 0, 0, 0});
+            weight_list[j][8].insert(weight_list[j][8].end(), {weights[i+j*cn][8], weights[i+j*cn][8], weights[i+j*cn][8], 0, weights[i+j*cn][8], weights[i+j*cn][8], weights[i+j*cn][8], 0, weights[i+j*cn][8], weights[i+j*cn][8], weights[i+j*cn][8], 0, 0, 0, 0, 0});
+        }
+    }
+
+    //    for (int i=0; i<co_cn; i++){
+    //        cout << "weights: " << i << endl;
+    //        for(int j=0; j<9; j++){
+    //            cout << "j: " << weight_list[i][j] << endl;
+    //        }
+    //    }
+
+    cc->EvalRotateKeyGen(keys.secretKey, {-5,-4,-3,-1,0,1,3,4,5,ih*iw});
+    vector<int> rot_index = {-5,-4,-3,-1,0,1,3,4,5};
+    vector<vector<vector<Ctxt>>> c_rotations(ci_cn, vector<vector<Ctxt>>(cn, vector<Ctxt>(9)));
+    for (int i=0; i<ci_cn; i++){
+        for (int j=0; j<cn; j++) {
+            for (int k=0; k<9; k++) {
+                c_rotations[i][j][k] = cc->EvalRotate(ct_inputs[i], rot_index[k]);
             }
-            Ctxt inter_conv_sum = cc->EvalAddMany(conv_ct);
-            inter_in_ct.push_back(inter_conv_sum);
+            ct_inputs[i] = cc->EvalRotate(ct_inputs[i], ih * iw);
+        }
+    }
 
-            Ptxt res;
-            cc->Decrypt(keys.secretKey, inter_in_ct[l], &res);
-            res->SetLength(numSlots);
-            cout << "out: " << l << endl;
-            cout << res << endl;
+    //    for (int i=0; i<ci_cn; i++){
+    //        for (int j=0; j<co; j++) {
+    //            cout << "ct: " << i << ", " << j << endl;
+    //            for (int k=0; k<9; k++) {
+    //                Ptxt res;
+    //                cc->Decrypt(keys.secretKey, c_rotations[i][j][k], &res);
+    //                res->SetLength(numSlots);
+    //                cout << "k: " << res;
+    //            }
+    //            cout << endl;
+    //        }
+    //    }
 
+    vector<Ctxt> inter_out_ct(co_cn);
+    for (int j = 0; j < co_cn; j++) {
+        vector<Ctxt> inter_in_ct;
+        for(int h=0; h < cn; h++){
+            for(int l=0; l < ci_cn; l++) {
+                vector<Ctxt> conv_ct;
+                for (int k = 0; k < 9; k++) {
+                    Ptxt weight_pt = cc->MakeCKKSPackedPlaintext(weight_list[j][k]);
+                    Ctxt cMult     = cc->EvalMult(weight_pt, c_rotations[l][h][k]);
+                    conv_ct.push_back(cMult);
+                }
+                Ctxt inter_conv_sum = cc->EvalAddMany(conv_ct);
+                inter_in_ct.push_back(inter_conv_sum);
+                //                Ptxt res;
+                //                cc->Decrypt(keys.secretKey, inter_conv_sum, &res);
+                //                res->SetLength(numSlots);
+                //                cout << "inter: " << endl;
+                //                cout << res << endl;
+            }
         }
         Ctxt inter_out_sum = cc->EvalAddMany(inter_in_ct);
-        inter_out_ct.push_back(inter_out_sum);
+        inter_out_ct[j] = inter_out_sum;
     }
 
-    for (int i=0; i<co; i++){
+    for (int i=0; i<co_cn; i++){
         Ptxt res;
         cc->Decrypt(keys.secretKey, inter_out_ct[i], &res);
         res->SetLength(numSlots);
